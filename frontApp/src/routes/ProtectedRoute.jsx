be32dabb8fdc3d9a6fdbe67f2ae9546bf7aaa556
GonzaloG28 <gonzalogimenez280@gmail.com>
2026-03-06 16:49:35 -0300

    variable entorno

--- frontApp/src/routes/ProtectedRoute.jsx
+++ frontApp/src/routes/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
-import { AuthContext } from "../context/authContext"
+import { AuthContext } from "../context/AuthContext"
 
 const ProtectedRouter = ({ children, allowedRoles }) => {
     const { isAuthenticated, loading, user } = useContext(AuthContext)
