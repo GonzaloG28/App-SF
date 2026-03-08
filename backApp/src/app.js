0a0e68d90a93b9ee37bafbeb0ab97396b2667473
GonzaloG28 <gonzalogimenez280@gmail.com>
2026-03-08 13:42:31 -0300

    arreglo cors

--- backApp/src/app.js
+++ backApp/src/app.js
@@ -44,7 +44,7 @@ app.use(cors({
 }));
 
 // Responder a peticiones OPTIONS de forma global
-app.options('*', cors());
+app.options('(.*)', cors());
 
 // --- MIDDLEWARES DE PARSEO ---
 app.use(express.json({ limit: '10mb' }));
